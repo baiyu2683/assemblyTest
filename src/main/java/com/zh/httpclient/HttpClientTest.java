1bda809ff3607f5aadce1f9e3b82693e1e0ef1f2
zhangheng2683 <zhangheng2683@gmail.com>
2017-07-17 02:24:07 +0800

    xx

---
+++ src/main/java/com/zh/httpclient/HttpClientTest.java
@@ -0,0 +1,10 @@
+package com.zh.httpclient;
+
+/**
+ * Created by zh on 2017-06-25.
+ */
+public class HttpClientTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/zh/kafka/KafkaTester.java
@@ -0,0 +1,10 @@
+package com.zh.kafka;
+
+/**
+ * Created by zh on 2017-07-16.
+ */
+public class KafkaTester {
+    public static void main(String[] args) {
+
+    }
+}
